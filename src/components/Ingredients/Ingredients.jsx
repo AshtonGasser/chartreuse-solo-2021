e5be0014b85664edd88c19048d39e335866338ef
Ashton Gasser <ashtongasser@gmail.com>
2021-06-29 14:13:00 -0500

    material table created

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
+import { DataGrid } from '@material-ui/data-grid';
+// import {
+//   randomCreatedDate,
+//   randomTraderName,
+//   randomUpdatedDate,
+// } from '@material-ui/x-grid-data-generator';
 function Ingredients () {
     const history = useHistory();
     const dispatch = useDispatch();
@@ -22,24 +28,59 @@ const handleBack = () => {
     console.log('clicked back to dash');
     history.push('/user')
 }
+const columns = [
+    { field: 'name', headerName: 'Name', width: 180, editable: true },
+    { field: 'ingredient type', headerName: 'ingredient_type', value: 'text', editable: true },
+     
+    {
+      field: 'description',
+      headerName: 'Description',
+      type: 'character varying(500)',
+      width: 220,
+      editable: true,
+    },
+    {
+      field: 'dateCreated',
+      headerName: 'Date Created',
+      type: 'date',
+      width: 180,
+      editable: true,
+    },
+   
+  ];
+  const rows = [
+    {
+      id: 1,
+      name: 'john snow',
+      age: 25,
+      dateCreated: 11/11/95,
+      lastLogin: 12/31/93,
+    },
+    ]
+
 return (
-    <>
-    <div>
-        <div>
-            <p>testing</p>
-        </div>
-    <section className ="ingredients">
-        {ingredient.map((ingredients => {
-            return (
-                <div key = {ingredients.id}><ul><li>{ingredients.name}</li></ul></div>
-            )
-        }))}
-
-    </section>
+    <div style={{ display: 'flex', height: '100%' }}>
+    <div style={{ flexGrow: 1 }}>
+    <div style={{ height: 300, width: '100%' }}>
+      <DataGrid rows={rows} columns={columns} />
     </div>
-    </>
-)
+    </div>
+  </div>
+  
+    
+  
 
+    //     {/* <section className ="ingredients">
+    //     {ingredient.map((ingredients => {
+    //         return (
+    //             <div key = {ingredients.id}><ul><li>{ingredients.name}</li></ul></div>
+    //         )
+    //     }))}
+
+    // </section> */}
+    
+    
+)
 
 }
 
