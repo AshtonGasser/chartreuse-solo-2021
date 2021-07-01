9cf9a98e12a9a127c84608c38b5f0de9b94fa8ee
Ashton Gasser <ashtongasser@gmail.com>
2021-07-01 15:24:44 -0500

    sheeesh

---
+++ src/components/IngredientTable/IngredientTable.jsx
@@ -0,0 +1,41 @@
+// import React, {useEffect, useState} from 'react';
+// import PropTypes from 'prop-types';
+// import clsx from 'clsx';
+// import { lighten, makeStyles } from '@material-ui/core/styles';
+// import Table from '@material-ui/core/Table';
+// import TableBody from '@material-ui/core/TableBody';
+// import TableCell from '@material-ui/core/TableCell';
+// import TableContainer from '@material-ui/core/TableContainer';
+// import TableHead from '@material-ui/core/TableHead';
+// import TablePagination from '@material-ui/core/TablePagination';
+// import TableRow from '@material-ui/core/TableRow';
+// import TableSortLabel from '@material-ui/core/TableSortLabel';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// import Paper from '@material-ui/core/Paper';
+// import Checkbox from '@material-ui/core/Checkbox';
+// import IconButton from '@material-ui/core/IconButton';
+// import Tooltip from '@material-ui/core/Tooltip';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Switch from '@material-ui/core/Switch';
+// import DeleteIcon from '@material-ui/icons/Delete';
+// import FilterListIcon from '@material-ui/icons/FilterList';
+// import { useDispatch, useSelector } from "react-redux";
+// import { useHistory, useParams } from "react-router-dom";
+// import { DataGrid } from "@material-ui/data-grid";
+// import { makeStyles } from "@material-ui/core/styles";
+// import TextField from "@material-ui/core/TextField";
+// import Button from "@material-ui/core/Button";
+// import InputLabel from "@material-ui/core/InputLabel";
+// import FormHelperText from "@material-ui/core/FormHelperText";
+// import FormControl from "@material-ui/core/FormControl";
+// import Select from "@material-ui/core/Select";
+// import DeleteIcon from '@material-ui/icons/Delete';
+// import EditIcon from '@material-ui/icons/Edit'
+
+// export default ({data, header}) =>
+// <Table>
+//     <TableHeader>
+
+//     </TableHeader>
+// </Table>
